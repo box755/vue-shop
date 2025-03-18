f76cfa343e8ae56249d36a97014b0522101d2b40
box755 <kk20031022@gmail.com>
2025-03-18 11:52:00 +0800

    sku refactor

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import imageView from '@/components/imageView/index.vue'
-import sku from '@/components/XtxSku/index.vue'
+import sku from '@/components/VueSku/index.vue'
 export const componentPlugin = {
   install(app) {
     app.component('ImageView', imageView);
